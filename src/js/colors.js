23c1106a0ea0bcc7fdcebfe70b0d255fe8268963
Ризобек Бабажанов <nazimaibragimova7582@gmail.com>
2025-03-26 20:46:32 +0500

    Overview

---
+++ src/js/colors.js
@@ -0,0 +1,323 @@
+$(document).ready(function() {
+    const color1 = document.getElementById('color1');
+    const color2 = document.getElementById('color2');
+    const color3 = document.getElementById('color3');
+    const color4 = document.getElementById('color4');
+    const color5 = document.getElementById('color5');
+    const color6 = document.getElementById('color6');
+    const color7 = document.getElementById('color7');
+    const color8 = document.getElementById('color8');
+    const color9 = document.getElementById('color9');
+    const color10 = document.getElementById('color10');
+    const color11 = document.getElementById('color11');
+    const color12 = document.getElementById('color12');
+    const color13 = document.getElementById('color13');
+    const color14 = document.getElementById('color14');
+    const color15 = document.getElementById('color15');
+    const color16 = document.getElementById('color16');
+    const color17 = document.getElementById('color17');
+    const color18 = document.getElementById('color18');
+    const color19 = document.getElementById('color19');
+    const color20 = document.getElementById('color20');
+    const color21 = document.getElementById('color21');
+    const color22 = document.getElementById('color22');
+    const color23 = document.getElementById('color23');
+    const color24 = document.getElementById('color24');
+    const color25 = document.getElementById('color25');
+    const color26 = document.getElementById('color26');
+
+    color1.addEventListener('mouseenter', () => {
+      color1.style.color = '#009EFF';
+      color1.style.transition = '0.4s'
+      color2.style.borderColor = '#009EFF';
+      color2.style.transition = '0.4s'
+      color3.style.borderColor = '#009EFF';
+      color3.style.transition = '0.4s'
+    });
+  
+    color1.addEventListener('mouseleave', () => {
+      color1.style.color = '#999999';
+      color2.style.borderColor = '#999999';
+      color3.style.borderColor = '#999999';
+    });
+
+    color4.addEventListener('mouseenter', () => {
+      color4.style.color = '#009EFF';
+      color4.style.transition = '0.4s'
+      color5.style.borderColor = '#009EFF';
+      color5.style.transition = '0.4s'
+      color6.style.borderColor = '#009EFF';
+      color6.style.transition = '0.4s'
+      color7.style.borderColor = '#009EFF';
+      color7.style.transition = '0.4s'
+    });
+  
+    color4.addEventListener('mouseleave', () => {
+      color4.style.color = '#999999';
+      color5.style.borderColor = '#999999';
+      color6.style.borderColor = '#999999';
+      color7.style.borderColor = '#999999';
+    });
+
+    color8.addEventListener('mouseenter', () => {
+        color8.style.color = '#009EFF';
+        color8.style.transition = '0.4s'
+        color9.style.borderColor = '#009EFF';
+        color9.style.transition = '0.4s'
+        color10.style.borderColor = '#009EFF';
+        color10.style.transition = '0.4s'
+      });
+    
+    color8.addEventListener('mouseleave', () => {
+        color8.style.color = '#999999';
+        color9.style.borderColor = '#999999';
+        color10.style.borderColor = '#999999';
+      });
+
+    color11.addEventListener('mouseenter', () => {
+        color11.style.color = '#009EFF';
+        color11.style.transition = '0.4s'
+        color12.style.borderColor = '#009EFF';
+        color12.style.transition = '0.4s'
+        color13.style.borderColor = '#009EFF';
+        color13.style.transition = '0.4s'
+      });
+    
+    color11.addEventListener('mouseleave', () => {
+        color11.style.color = '#999999';
+        color12.style.borderColor = '#999999';
+        color13.style.borderColor = '#999999';
+      });
+
+    color14.addEventListener('mouseenter', () => {
+        color14.style.color = '#009EFF';
+        color14.style.transition = '0.4s'
+        color15.style.borderColor = '#009EFF';
+        color15.style.transition = '0.4s'
+        color16.style.borderColor = '#009EFF';
+        color16.style.transition = '0.4s'
+      });
+    
+    color14.addEventListener('mouseleave', () => {
+        color14.style.color = '#999999';
+        color15.style.borderColor = '#999999';
+        color16.style.borderColor = '#999999';
+      });
+
+    color17.addEventListener('mouseenter', () => {
+        color17.style.color = '#009EFF';
+        color17.style.transition = '0.4s'
+        color18.style.borderColor = '#009EFF';
+        color18.style.transition = '0.4s'
+        color19.style.borderColor = '#009EFF';
+        color19.style.transition = '0.4s'
+      });
+    
+    color17.addEventListener('mouseleave', () => {
+        color17.style.color = '#999999';
+        color18.style.borderColor = '#999999';
+        color19.style.borderColor = '#999999';
+      });
+
+    color20.addEventListener('mouseenter', () => {
+        color20.style.color = '#009EFF';
+        color20.style.transition = '0.4s'
+        color21.style.borderColor = '#009EFF';
+        color21.style.transition = '0.4s'
+        color22.style.borderColor = '#009EFF';
+        color22.style.transition = '0.4s'
+      });
+    
+    color20.addEventListener('mouseleave', () => {
+        color20.style.color = '#999999';
+        color21.style.borderColor = '#999999';
+        color22.style.borderColor = '#999999';
+      });
+
+    color23.addEventListener('mouseenter', () => {
+        color23.style.color = '#009EFF';
+        color23.style.transition = '0.4s'
+        color24.style.borderColor = '#009EFF';
+        color24.style.transition = '0.4s'
+        color25.style.borderColor = '#009EFF';
+        color25.style.transition = '0.4s'
+        color26.style.borderColor = '#009EFF';
+        color26.style.transition = '0.4s'
+      });
+    
+    color23.addEventListener('mouseleave', () => {
+        color23.style.color = '#999999';
+        color24.style.borderColor = '#999999';
+        color25.style.borderColor = '#999999';
+        color26.style.borderColor = '#999999';
+      });
+});
+
+$(document).ready(function() {
+  const roloc1 = document.getElementById('roloc1');
+  const roloc2 = document.getElementById('roloc2');
+  const roloc3 = document.getElementById('roloc3');
+  const roloc4 = document.getElementById('roloc4');
+  const roloc5 = document.getElementById('roloc5');
+  const roloc6 = document.getElementById('roloc6');
+  const roloc7 = document.getElementById('roloc7');
+  const roloc8 = document.getElementById('roloc8');
+  const roloc9 = document.getElementById('roloc9');
+  const roloc10 = document.getElementById('roloc10');
+  const roloc11 = document.getElementById('roloc11');
+  const roloc12 = document.getElementById('roloc12');
+  const roloc13 = document.getElementById('roloc13');
+  const roloc14 = document.getElementById('roloc14');
+  const roloc15 = document.getElementById('roloc15');
+  const roloc16 = document.getElementById('roloc16');
+  const roloc17 = document.getElementById('roloc17');
+  const roloc18 = document.getElementById('roloc18');
+  const roloc19 = document.getElementById('roloc19');
+  const roloc20 = document.getElementById('roloc20');
+  const roloc21 = document.getElementById('roloc21');
+  const roloc22 = document.getElementById('roloc22');
+  const roloc23 = document.getElementById('roloc23');
+  const roloc24 = document.getElementById('roloc24');
+  const roloc25 = document.getElementById('roloc25');
+  const roloc26 = document.getElementById('roloc26');
+  const roloc27 = document.getElementById('roloc27');
+  const roloc28 = document.getElementById('roloc28');
+
+  roloc1.addEventListener('mouseenter', () => {
+    roloc1.style.color = '#009EFF';
+    roloc1.style.transition = '0.4s'
+    roloc2.style.borderColor = '#009EFF';
+    roloc2.style.transition = '0.4s'
+    roloc3.style.borderColor = '#009EFF';
+    roloc3.style.transition = '0.4s'
+    roloc4.style.borderColor = '#009EFF';
+    roloc4.style.transition = '0.4s'
+  });
+
+  roloc1.addEventListener('mouseleave', () => {
+    roloc1.style.color = '#999999';
+    roloc2.style.borderColor = '#999999';
+    roloc3.style.borderColor = '#999999';
+    roloc4.style.borderColor = '#999999';
+  });
+
+  roloc5.addEventListener('mouseenter', () => {
+    roloc5.style.color = '#009EFF';
+    roloc5.style.transition = '0.4s'
+    roloc6.style.borderColor = '#009EFF';
+    roloc6.style.transition = '0.4s'
+    roloc7.style.borderColor = '#009EFF';
+    roloc7.style.transition = '0.4s'
+  });
+
+  roloc5.addEventListener('mouseleave', () => {
+    roloc5.style.color = '#999999';
+    roloc6.style.borderColor = '#999999';
+    roloc7.style.borderColor = '#999999';
+  });
+
+  roloc8.addEventListener('mouseenter', () => {
+    roloc8.style.color = '#009EFF';
+    roloc8.style.transition = '0.4s'
+    roloc9.style.borderColor = '#009EFF';
+    roloc9.style.transition = '0.4s'
+    roloc10.style.borderColor = '#009EFF';
+    roloc10.style.transition = '0.4s'
+  });
+
+  roloc8.addEventListener('mouseleave', () => {
+    roloc8.style.color = '#999999';
+    roloc9.style.borderColor = '#999999';
+    roloc10.style.borderColor = '#999999';
+  });
+
+  roloc11.addEventListener('mouseenter', () => {
+    roloc11.style.color = '#009EFF';
+    roloc11.style.transition = '0.4s'
+    roloc12.style.borderColor = '#009EFF';
+    roloc12.style.transition = '0.4s'
+    roloc13.style.borderColor = '#009EFF';
+    roloc13.style.transition = '0.4s'
+  });
+
+  roloc11.addEventListener('mouseleave', () => {
+    roloc11.style.color = '#999999';
+    roloc12.style.borderColor = '#999999';
+    roloc13.style.borderColor = '#999999';
+  });
+
+  roloc14.addEventListener('mouseenter', () => {
+    roloc14.style.color = '#009EFF';
+    roloc14.style.transition = '0.4s'
+    roloc15.style.borderColor = '#009EFF';
+    roloc15.style.transition = '0.4s'
+    roloc16.style.borderColor = '#009EFF';
+    roloc16.style.transition = '0.4s'
+  });
+
+  roloc14.addEventListener('mouseleave', () => {
+    roloc14.style.color = '#999999';
+    roloc15.style.borderColor = '#999999';
+    roloc16.style.borderColor = '#999999';
+  });
+
+  roloc17.addEventListener('mouseenter', () => {
+    roloc17.style.color = '#009EFF';
+    roloc17.style.transition = '0.4s'
+    roloc18.style.borderColor = '#009EFF';
+    roloc18.style.transition = '0.4s'
+    roloc19.style.borderColor = '#009EFF';
+    roloc19.style.transition = '0.4s'
+  });
+
+  roloc17.addEventListener('mouseleave', () => {
+    roloc17.style.color = '#999999';
+    roloc18.style.borderColor = '#999999';
+    roloc19.style.borderColor = '#999999';
+  });
+
+  roloc20.addEventListener('mouseenter', () => {
+    roloc20.style.color = '#009EFF';
+    roloc20.style.transition = '0.4s'
+    roloc21.style.borderColor = '#009EFF';
+    roloc21.style.transition = '0.4s'
+    roloc22.style.borderColor = '#009EFF';
+    roloc22.style.transition = '0.4s'
+  });
+
+  roloc20.addEventListener('mouseleave', () => {
+    roloc20.style.color = '#999999';
+    roloc21.style.borderColor = '#999999';
+    roloc22.style.borderColor = '#999999';
+  });
+
+  roloc23.addEventListener('mouseenter', () => {
+    roloc23.style.color = '#009EFF';
+    roloc23.style.transition = '0.4s'
+    roloc24.style.borderColor = '#009EFF';
+    roloc24.style.transition = '0.4s'
+    roloc25.style.borderColor = '#009EFF';
+    roloc25.style.transition = '0.4s'
+  });
+
+  roloc23.addEventListener('mouseleave', () => {
+    roloc23.style.color = '#999999';
+    roloc24.style.borderColor = '#999999';
+    roloc25.style.borderColor = '#999999';
+  });
+
+  roloc26.addEventListener('mouseenter', () => {
+    roloc26.style.color = '#009EFF';
+    roloc26.style.transition = '0.4s'
+    roloc27.style.borderColor = '#009EFF';
+    roloc27.style.transition = '0.4s'
+    roloc28.style.borderColor = '#009EFF';
+    roloc28.style.transition = '0.4s'
+  });
+
+  roloc26.addEventListener('mouseleave', () => {
+    roloc26.style.color = '#999999';
+    roloc27.style.borderColor = '#999999';
+    roloc28.style.borderColor = '#999999';
+  });
+});
